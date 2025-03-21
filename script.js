58e8cfef99ac73dff94c1cc248dfd736ee02136e
lihu levit <99426683+Lihu0@users.noreply.github.com>
2025-03-21 23:09:58 +0200

    Made the R-G-B seperating use collumsinsted of rows

--- script.js
+++ script.js
@@ -67,46 +67,43 @@ document.getElementById("download").addEventListener("click", function () {
   const ws = {};
   const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
   const data = imageData.data;
+
   let refStartColumn = "A";
-  let refEndColumn = "A";
+  let refEndColumn = numberToExcelColumn(canvas.width);
   let refStartRow = 1;
-  let refEndRow = 1;
+  let refEndRow = canvas.height * 3;
+
   for (let i = 0; i < canvas.width; i++) {
     for (let j = 0; j < canvas.height; j++) {
       const index = (j * canvas.width + i) * 4;
       const red = data[index];
       const green = data[index + 1];
       const blue = data[index + 2];
-      const cellRed = numberToExcelColumn(i * 3 + 1) + (j + 1);
-      ws[cellRed] = {
+
+      const cellColumn = numberToExcelColumn(i + 1);
+
+      ws[cellColumn + (j * 3 + 1)] = {
         t: "n",
         s: { fill: { fgColor: { rgb: rgbToHex(red, 0, 0) } } },
         v: red,
       };
-      const cellGreen = numberToExcelColumn(i * 3 + 2) + (j + 1);
-      ws[cellGreen] = {
+
+      ws[cellColumn + (j * 3 + 2)] = {
         t: "n",
         s: { fill: { fgColor: { rgb: rgbToHex(0, green, 0) } } },
         v: green,
       };
-      const cellBlue = numberToExcelColumn(i * 3 + 3) + (j + 1);
-      ws[cellBlue] = {
+
+      ws[cellColumn + (j * 3 + 3)] = {
         t: "n",
         s: { fill: { fgColor: { rgb: rgbToHex(0, 0, blue) } } },
         v: blue,
       };
-      const lastColumn = i * 3 + 3;
-      if (lastColumn > refEndColumn.charCodeAt(0) - 64) {
-        refEndColumn = numberToExcelColumn(lastColumn);
-      }
-      refEndRow = j + 1;
     }
   }
+
   ws["!ref"] = `${refStartColumn}${refStartRow}:${refEndColumn}${refEndRow}`;
-  const numColumns = canvas.width * 3;
-  ws["!cols"] = Array(numColumns).fill({ wch: 2.75 });
-  const numRows = canvas.height;
-  ws["!rows"] = Array(numRows).fill({ hpt: 50 });
+
   const wb = { SheetNames: ["Sheet1"], Sheets: { Sheet1: ws } };
   XLSX.writeFile(wb, "image.xlsx");
 });
